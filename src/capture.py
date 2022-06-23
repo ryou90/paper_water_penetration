d63cb6bec17b24e07e407a71bf07521a2edb8543
Ryou90 <r.unger@online.de>
2022-06-23 02:19:13 +0200

    Adds image handler class

--- src/capture.py
+++ src/capture.py
@@ -1,22 +1,14 @@
 import pathlib
 import time
-from os import listdir
-from os.path import isfile, join
 from typing import Any, List, Optional, Tuple
 
 import cv2 as cv
 
+from image import Images
+
 
 def current_milli_time() -> int:
     return int(round(time.time() * 1000))
-
-
-def get_current_path() -> str:
-    return str(pathlib.Path(__file__).parent.absolute())
-
-
-def create_path(path: str) -> None:
-    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
 
 
 class Capture:
@@ -32,6 +24,7 @@ class Capture:
         intervals: List[Tuple[int, int]] = [(1000, 100)],
         filename: Optional[str] = None,
         path: Optional[str] = None,
+        image_handler: Images = Images(),
     ) -> None:
         """
         Configure instance
@@ -39,13 +32,7 @@ class Capture:
         interval: List of Tuple(capture duration, break between two images)
         """
         self.intervals = intervals
-        if filename:
-            self.filename = filename
-
-        self.path: str = path if path else get_current_path()
-        create_path(self.path)
-
-        self.fullname = pathlib.Path(self.path, self.filename)
+        self.images = image_handler
 
     def run_capture(self, convert: bool = False) -> None:
         """Run caputering and image creating process"""
@@ -72,14 +59,14 @@ class Capture:
                     if ret:
                         # Create name
                         name: str = "%s%d%s" % (
-                            self.fullname,
+                            self.images.fullname,
                             image_count,
-                            self.file_ending,
+                            self.images.file_ending,
                         )
                         if convert:
                             frame = Capture.convert_image_to_gray(frame)
                         # convert and save image to disk
-                        Capture.save_image(frame, name)
+                        self.images.save(frame, name)
                         image_count += 1
 
                 # calc difference from start to now
@@ -88,40 +75,11 @@ class Capture:
         # Release camera
         cap.release()
 
-    def run_convert(self, replace: bool = False) -> None:
+    def run_convert(self, replace: bool = False) -> List[Any]:
         """Read image files and batch convert them"""
-        # First get all file names
-        files = [
-            join(self.path, f)
-            for f in listdir(self.path)
-            if isfile(join(self.path, f)) and f.startswith(self.filename)
-        ]
-
-        file: Any
-        name: str
-        for child in pathlib.Path(self.path).rglob("*%s" % self.file_ending):
-            if child.is_file() and child.name.startswith(self.filename):
-                # Read image
-                file = cv.imread(str(child))
-                # if replace image with converted image
-                if replace:
-                    # convert and replace image data
-                    name = child.name
-                else:
-                    # create new name from child
-                    name = join(
-                        self.path,
-                        child.name.replace(self.file_ending, ""),
-                        "_gray",
-                        self.file_ending,
-                    )
-                    Capture.save_image(Capture.convert_image_to_gray(file), name)
-
-    @classmethod
-    def save_image(cls, frame: Any, name: str) -> None:
-        """Save frame as image with name"""
-        # save image
-        cv.imwrite(name, frame)
+        return self.images.batch_transform(
+            Capture.convert_image_to_gray, replace, transform_suffix="_gray"
+        )
 
     @classmethod
     def convert_image_to_gray(cls, image: Any) -> Any:
---
+++ src/image.py
@@ -0,0 +1,87 @@
+import pathlib
+
+from os.path import join
+from typing import Any, Callable, List, Optional
+import cv2 as cv
+
+def get_current_path() -> str:
+    return str(pathlib.Path(__file__).parent.absolute())
+
+
+def create_path(path: str) -> None:
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
+class Images:
+    filename: str = "DVM22_Penetration_"
+    file_ending: str = ".jpg"
+
+    def __init__(
+        self,
+        filename: Optional[str] = None,
+        path: Optional[str] = None,
+    ) -> None:
+        """
+        Configure instance
+        """
+
+        if filename:
+            self.filename = filename
+
+        self.path: str = path if path else get_current_path()
+        create_path(self.path)
+
+        self.fullname = pathlib.Path(self.path, self.filename)
+
+        self._images : List[Any] = []
+
+    @property
+    def images(self) -> List[Any]:
+        """Return cached list from last operation"""
+        return self._images
+
+    def batch_read(self) -> List[Any]:
+        self._images = []
+        for child in pathlib.Path(self.path).rglob("*%s" % self.file_ending):
+            if child.is_file() and child.name.startswith(self.filename):
+                # Read image
+                self._images.append(cv.imread(str(child)))
+
+        return self._images
+
+    def batch_transform(self, transformer_fn: Callable, replace: bool = False, transform_suffix : str = "_transform") -> List[Any]:
+        file: Any
+        name: str
+        self._images = []
+
+        for child in pathlib.Path(self.path).rglob("*%s" % self.file_ending):
+            if child.is_file() and child.name.startswith(self.filename):
+                # Read image
+                file = cv.imread(str(child))
+                # if replace image with converted image
+                if replace:
+                    # convert and replace image data
+                    name = child.name
+                else:
+                    # create new name from child
+                    name = join(
+                        self.path,
+                        child.name.replace(self.file_ending, ""),
+                        transform_suffix,
+                        self.file_ending,
+                    )
+
+                    # Call transformer function
+                    file = transformer_fn(file)
+                    # Add transformed image to list
+                    self._images.append(file)
+                    # save image
+                    self.save(file, name)
+
+        # return list
+        return self._images
+
+
+    def save(self, image: Any, name: str) -> None:
+        """Save frame as image with name"""
+        # save image
+        cv.imwrite(name, image)
