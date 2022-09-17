82beb347eb723bea29169a38f4b3a930caa50ad8
Ryou90 <r.unger@online.de>
2022-09-17 15:57:23 +0200

    Wip missing run.py

--- src/run.py
+++ src/run.py
@@ -5,10 +5,17 @@ log = get_logger("Script")
 log.setLevel(logging.INFO)
 
 def start():
-    log.info("Start Paper Water Pentration Script")
+    log.info("Start Paper Water Pentration")
     pwp = PaperWaterPenetration()
     pwp.run()
     log.info("Finish")
+
+
+def capture():
+    log.info("Start Paper Water Pentration Capturing")
+    pwp = PaperWaterPenetration()
+    pwp.capture()
+    log.info("Finish capturing")
 
 
 if __name__ == "__main__":
